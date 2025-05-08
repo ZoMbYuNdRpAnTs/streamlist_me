3cf392bcd70e7d39e28027bae61f917c09bde8bb
ZoMbYuNdRpAnTs <145167321+ZoMbYuNdRpAnTs@users.noreply.github.com>
2025-05-07 21:41:25 -0600

    fine tuning

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-        <head />
+        <head ><title></title></head>
         <body>
         <div className="app-layout">
             <nav className="sidebar">
